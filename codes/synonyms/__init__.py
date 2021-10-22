56f75b2db317f95611b618603aa0718548f2122d
lilijuan11 <lilijuan11@hikvision.com.cn>
2021-10-22 16:51:37 +0800

    update:中文EDA数据增强

---
+++ codes/synonyms/__init__.py
@@ -0,0 +1,15 @@
+__all__ = ["seg",
+    "nearby", 
+    "compare", 
+    "display", 
+    "keywords",
+    "KeyedVectors", 
+    "any2utf8",
+    "sigmoid",
+    "cosine",
+    "any2unicode",
+    "__version__"]
+
+from .word2vec import *
+from .synonyms import *
+from .synonyms import __version__
